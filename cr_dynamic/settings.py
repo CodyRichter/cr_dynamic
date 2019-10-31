d0cf902dbdfeb2ee60e6fdf14d58f83a8066a26f
Senarii <Vortekdynamic@gmail.com>
2019-10-31 17:59:39 -0400

    Update more front-end styles

--- cr_dynamic/settings.py
+++ cr_dynamic/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.conf.global_settings import AUTH_USER_MODEL
+from django.contrib.messages import constants as messages
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -112,6 +114,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
